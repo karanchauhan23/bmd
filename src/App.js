651899759d7594572fe31aa2907f29f8a693b667
karanchauhan23 <98728627+karanchauhan23@users.noreply.github.com>
2022-01-31 11:11:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+// import SignUp from './views/SignUp';
+// import SignIn from './views/SignIn';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+import Homepage from './views/Homepage';
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+      {/* <SignUp /> */}
+      {/* <SignIn /> */}
+      {/* <Header /> */}
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
