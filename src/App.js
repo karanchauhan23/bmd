abba244265e9d76b2f309aadd80dbc7677996ed6
karanchauhan23 <98728627+karanchauhan23@users.noreply.github.com>
2022-02-09 12:31:02 +0530

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
 import "./App.css";
-
-// Router
-import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
-
-// Redux
-import { Provider } from "react-redux";
-import store from "./Store/store";
-
-// Components
-import SignUp from "./views/SignUp";
-import SignIn from "./views/SignIn";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Homepage from "./views/Homepage";
-import Contact from "./views/Contact";
+// import SignUp from './views/SignUp';
+// import SignIn from "./views/SignIn";
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+// import Homepage from "./views/Homepage";
+// import Contact from "./views/Contact";
+// import Lost from './views/Lost';
+import Win from './views/Win';
 
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </Provider>
+    <div className="App">
+      {/* <Homepage /> */}
+      {/* <SignUp /> */}
+      {/* <Contact /> */}
+      {/* <SignIn /> */}
+      {/* <Header /> */}
+      {/* <Footer /> */}
+      <Win />
+      {/* <Lost /> */}
+    </div>
   );
 }
 
